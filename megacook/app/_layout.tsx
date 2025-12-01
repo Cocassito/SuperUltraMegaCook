4b4ef9dda4b5fd76940baac2ddde15ac3d8366b6
Gautier FOURNET <gautier.fournet@gmail.com>
2025-12-01 15:11:18 +0100

    feat : Change Screen Orientation

--- megacook/app/_layout.tsx
+++ megacook/app/_layout.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Stack } from 'expo-router';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function RootLayout() {
+  useEffect(() => {
+    async function lockOrientation() {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.LANDSCAPE
+      );
+    }
+    lockOrientation();
+  }, []);
+
   return (
     <Stack screenOptions={{ headerShown: false }} />
   );
