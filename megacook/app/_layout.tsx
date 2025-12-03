b089e906636f65bce60924c7f810c140c528d025
Alan <king.alan44@gmail.com>
2025-12-03 16:47:54 +0100

    feat : add font

---
+++ megacook/app/_layout.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "pixelgridtrial-linedownboldm": require("../assets/fonts/pixelgridtrial-linedownboldm.otf"),
+  });
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
