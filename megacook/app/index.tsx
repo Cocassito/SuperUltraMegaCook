b34973cf502eacc33c8188e0c6f68ea466dda1bd
Alan <king.alan44@gmail.com>
2025-12-03 12:08:23 +0100

    feat : add homepage

--- megacook/app/index.tsx
+++ megacook/app/index.tsx
@@ -1,40 +1,10 @@
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { useRouter } from "expo-router";
-import { Image } from "expo-image";
-import PixelButton from "@/components/ui/button/PixelButton";
+import HomePage from "@/pages/HomePage";
 
 export default function Menu() {
-  const router = useRouter();
-
-  const handleStart = () => {
-    router.push("/App");
-  };
-
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("../assets/images/logo/Logo_MC_Complet.webp")}
-        style={styles.logo}
-        contentFit="contain"
-      />{" "}
-      <PixelButton label="Commencer" onPress={handleStart} />
-    </View>
+    <>
+      <HomePage />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FFF2DD",
-    gap: 4,
-  },
-  logo: {
-    width: 200,
-    height: 200,
-  },
-});
--- megacook/pages/HomePage.tsx
+++ megacook/pages/HomePage.tsx
@@ -1,3 +1,39 @@
+import { View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+import PixelButton from "@/components/ui/button/PixelButton";
+import { useRouter } from "expo-router";
+
 export default function HomePage() {
-  return <></>;
+  const router = useRouter();
+  const handleStart = () => {
+    router.push("/App");
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Image
+          source={require("../assets/images/logo/Logo_MC_Complet.webp")}
+          style={styles.logo}
+          contentFit="contain"
+        />{" "}
+        <PixelButton label="Commencer" onPress={handleStart} />
+      </View>
+    </>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFF2DD",
+    gap: 4,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
